ff2a0c3ac2fb096ed8fc8a7d00e74bde574a8364
zhen.chen <zhen.chen@newtouch.cn>
2016-09-29 12:46:56 +0800

    添加忘记密码

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include_once 'resource/session.php' ?>
 <!DOCTYPE html>
 <html>
 <head lang="en">
@@ -7,8 +8,11 @@
 <body>
 <h2>User Authentication System </h2><hr>
 
+<?php if(!isset($_SESSION['username'])): ?>
 <P>You are currently not signin <a href="login.php">Login</a> Not yet a member? <a href="signup.php">Signup</a> </P>
+<?php else: ?>
+<p>You are logged in as <?php if(isset($_SESSION['username'])) echo $_SESSION['username']; ?> <a href="logout.php">Logout</a> </p>
+<?php endif ?>
 
-<p>You are logged in as {username} <a href="logout.php">Logout</a> </p>
 </body>
 </html>
--- resource/utilities.php
+++ resource/utilities.php
@@ -27,7 +27,7 @@ function check_min_length($fields_to_check_length){
     $form_errors = array();
 
     foreach($fields_to_check_length as $name_of_field => $minimum_length_required){
-        if(strlen(trim($_POST[$name_of_field])) < $minimum_length_required){
+        if(strlen(trim($_POST[$name_of_field])) < $minimum_length_required && $_POST[$name_of_field] != NULL){
             $form_errors[] = $name_of_field . " is too short, must be {$minimum_length_required} characters long";
         }
     }
